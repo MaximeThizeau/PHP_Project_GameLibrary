1dcc9e37eb5c79a55fa747b0071260debbea6ef2
Thibault Denuc <thibault.denuc@y-nov.com>
2014-10-30 17:02:43 +0100

    début du site

---
+++ site/index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<head>
+	<title> GamerZ </title> <meta charset="utf-8" />
+    <link rel="stylesheet" media="screen" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/search.js"></script>
+</head>
+<body>
+	<div id="header">
+		<div id="content_header">
+			<div id="top_barre">
+				<div id="logo"> <p id="script"> Gamer</p> <p id="Z"> Z </p> </div>
+				<div id="space_1"> </div>
+				<div id="search_"> 
+					<form class="form-wrapper" method="post">
+					<div class="btn-left-loupe"></div>
+					<input class=".search_data" name="saisie" type="text" id="recherche" placeholder="Mots-Clefs..." />
+					<button type="submit">Search</button>
+					<div class="resultat" id="resultat"></div>
+					</form>
+				</div>
+				<div id="menu">
+					<ol>
+	                    <li>
+	                        <a href="#">Discovers</a>
+	                    </li>
+	                </ol>
+	                <div id="space_3"> </div>
+	                <ol>
+	                    <li>
+	                        <a href="#">Plateforms</a>
+	                    </li>
+	                </ol>
+	            </div>
+	            <div id="connection"> <img src="img/connection.png"> </div>
+	        </div>
+	    </div>
+    </div>
+
+
+    <div id="content_top_game">
+    	<div id="block_left">
+    		<div id="content_top_block_left">
+	    		<div id="img_content_top_block_left">
+	    			<div id="logo_block_left"> </div>
+	    		</div>
+	    	</div>
+	    		<div id="games_name"> </div>
+	    </div>
+	    <div id="block_right">
+	    	<div id="content_top_block_top"> Jeux similaire </div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+	    	<div class="content_middle_block_top_img"> <img src="img/AssassinScreed.png">
+	    		<div class="content_middle_block_top_title"><h5>Assasin's screed</h5></div>	
+	    	</div>
+		</div>
+		<div id="block_middle">
+	    	<div id="content_top_block_middle"> </div>
+	    	<div id="content_middle_block_middle"> </div>
+	    	<div id="content_bottom_block_middle"> </div>
+	    </div>
+	</div>
+
+
+	<div id="content_middle">
+		<div class="content_middle_left">
+			<div class="title_content"> Les plus populaires </div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+				<div class="content_middle_title"> Assassin's creed </div>
+				<div class="categories">
+					<div class="content_categories"> Aventure  </div>
+					<div class="content_categories"> Action </div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+		<div class="content_middle_right">
+			<div class="title_content"> Les plus populaires </div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+			<div class="block_content_game">
+				<div class="block_content_game_img"> <img src="img/AssassinScreedMiddle.png">
+					<div class="block_content_game_img_icon"> </div>
+				</div>
+			<div class="content_middle_title"> Assassin's creed </div>
+			<div class="categories">
+				<div class="content_categories"> Aventure </div>
+				<div class="content_categories"> Action </div>
+			</div>
+			</div>
+		</div>
+	</div>
+</body>		
